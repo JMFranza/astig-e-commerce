286c1dbf4aaaac8f2cdef7d97ee4e84a1952ea3b
jamie <49512208+JMFranza@users.noreply.github.com>
2022-03-27 11:41:08 +0800

    CategoryItem responsive to mobile

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styledComponents from "styled-components"
+import { mobile } from "../responsive";
 
 const Container = styledComponents.div`
     flex: 1;
@@ -11,6 +12,7 @@ const Image = styledComponents.img`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    ${mobile({ height: "20vh" })}
 `;
 
 const Info = styledComponents.div`
